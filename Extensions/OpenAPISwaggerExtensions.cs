97cd77d7b680b59ca430b82ffb36839e50295db2
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2026-03-17 09:45:03 -0500

    setup Swagger;

---
+++ Extensions/OpenAPISwaggerExtensions.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.OpenApi;
+
+namespace MinimalAPI2026Demo.Extensions
+{
+    public static class OpenAPISwaggerExtensions
+    {
+        public static IServiceCollection AddCustomSwagger(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1", new OpenApiInfo
+                    {
+                        Version = "v1",
+                        Title = "ASP.NET Core 10.0 Minimal API Demo",
+                        Description = """
+    
+    <img src="/images/AeonRegistryLogoBLK.png" height="120" />
+    
+    ## Aeon Research Division
+
+    Internal API for managing recovered artifacts and research data.
+    Provides secure access for field researchers and analysts. 
+
+    ### Key Features:
+    - Site and Artifact Catalog
+    - Research record submissions
+    - Secure media storage
+    - User role managment
+
+    [Launch Public Test Site](/site/sites-map.html)
+
+    """,
+        Contact = new OpenApiContact
+        {
+            Name = "SJ Wonder",
+            Url = new Uri("https://example.com"),
+            Email = "shelly.wonder@outlook.com"
+        }
+                    });
+
+                    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                    {
+                        Name = "Authorization",
+                        Type = SecuritySchemeType.Http,
+                        Scheme = "bearer",
+                        BearerFormat = "JWT",
+                        In = ParameterLocation.Header,
+                        Description = "Enter 'Bearer' [space] and then your valid JWT token."
+                    });
+
+                    c.AddSecurityRequirement(document =>new OpenApiSecurityRequirement
+                    {
+                        [new OpenApiSecuritySchemeReference("Bearer", document)] = []
+                    });
+                });
+            return services;
+        }
+    }
+}
---
+++ GlobalUsings.cs
@@ -0,0 +1,1 @@
+﻿global using MinimalAPI2026Demo.Extensions;
