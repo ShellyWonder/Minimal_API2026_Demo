f33d615810611e1cf6752f78d8063c1fdf6ec726
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2026-07-19 00:41:41 -0500

    Add public site endpoints and handler
    Added MinimalAPI2026Demo.Endpoints.Sites namespace with SiteEndpoints static class. Introduced MapSiteEndpoints extension for IEndpointRouteBuilder to register public site API endpoints. Created /api/public/sites group with metadata. Added GET endpoint for all public sites, handled by GetAllPublicSites, returning PublicSiteResponse list from ISiteService.

---
+++ Endpoints/Sites/SiteEndpoints.cs
@@ -0,0 +1,29 @@
+﻿
+namespace MinimalAPI2026Demo.Endpoints.Sites
+{
+    public static class SiteEndpoints
+    {
+        public static IEndpointRouteBuilder MapSiteEndpoints(this IEndpointRouteBuilder route)
+        {
+            var publicGroup = route.MapGroup("/api/public/sites")
+               .AllowAnonymous()
+                .WithSummary("Public Site Endpoints.")
+                .WithDescription("Displays a list of all publically available site data")
+                .WithTags("Sites - Public");
+
+            publicGroup.MapGet("", GetAllPublicSites)
+                .WithName(nameof(GetAllPublicSites))
+                .Produces<List<PublicSiteResponse>>(StatusCodes.Status200OK)
+                .WithSummary("Public Site Endpoints.")
+                .WithDescription("Returns a list of all publically available site data");
+
+            return route;
+        }
+        #region Handlers
+        private static async Task<Ok<List<PublicSiteResponse>>> GetAllPublicSites(ISiteService service, CancellationToken ct)
+        {
+            return TypedResults.Ok(await service.GetAllSitesPublicAsync(ct));
+        }
+        #endregion
+    }
+}
