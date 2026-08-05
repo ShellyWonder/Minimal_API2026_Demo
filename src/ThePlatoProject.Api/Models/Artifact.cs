5d98549956cb01c30a28c52d1f2c502d1a704f15
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2026-08-05 11:25:03 -0500

    Refactor artifact archival to custody/transfer model
    Replaces archival properties with new custody and transfer fields.

--- src/ThePlatoProject.Api/Models/Artifact.cs
+++ src/ThePlatoProject.Api/Models/Artifact.cs
@@ -32,54 +32,43 @@
 
         public ApplicationUser? VerifiedBy { get; set; }
 
-        public ArchiveWorkflowState ArchiveState { get; set; }
-                                        = ArchiveWorkflowState.None;
+        public ArtifactCustodyStatus CustodyStatus { get; set; }
+                                        = ArtifactCustodyStatus.OnSite;
+        [MaxLength(2500)]
+        public string? TransferReason { get; set; }
 
-        [MaxLength(1000)]
-        public string? ArchiveReason { get; set; }
+        public DateTimeOffset? TransferRequestedAtUtc { get; set; }
+        public string? TransferRequestedById { get; set; }
+        public ApplicationUser? TransferRequestedBy { get; set; }
 
-        public DateTimeOffset? ArchiveRequestedAtUtc { get; set; }
+        public DateTimeOffset? TransferAuthorizedAtUtc { get; set; }
+        public string? TransferAuthorizedById { get; set; }
+        public ApplicationUser? TransferAuthorizedBy { get; set; }
 
-        public string? ArchiveRequestedById { get; set; }
-
-        public DateTimeOffset? ArchiveAuthorizedAtUtc { get; set; }
-
-        public string? ArchiveAuthorizedById { get; set; }
-
-        public DateTimeOffset? ArchivedAtUtc { get; set; }
-
-        public string? ArchivedById { get; set; }
-
-        [Required]
-        public int SiteId { get; set; } //foreign key
-        public Site? Site { get; set; } //Nav property
-
-        //archive navigation properties
-        public ApplicationUser? ArchiveRequestedBy { get; set; }
-
-        public ApplicationUser? ArchiveAuthorizedBy { get; set; }
-
-        public ApplicationUser? ArchivedBy { get; set; }
-
-        //Nav properties 
-        public List<ArtifactMediaFile> MediaFiles { get; set; } = [];
-        public List<CatalogRecord> CatalogRecords { get; set; } = [];
-
-        //Custody properties tracking transfers of the artifact
+        //Custodial properties tracking transfers of the artifact
         public DateTimeOffset? ShipmentSentAtUtc { get; set; }
-
         public string? ShipmentSentById { get; set; }
 
-        public DateTimeOffset? ShipmentReceivedAtUtc { get; set; }
+        
 
+        public DateTimeOffset? ShipmentReceivedAtUtc { get; set; }
         public string? ShipmentReceivedById { get; set; }
 
         [MaxLength(250)]
         public string? WarehouseLocation { get; set; }
 
+        [Required]
+        public int SiteId { get; set; } //foreign key
+        public Site? Site { get; set; } //Nav property
+
+
         //Nav properties for custody tracking
         public ApplicationUser? ShipmentSentBy { get; set; }
+        public ApplicationUser? ShipmentReceivedBy { get; set; } 
 
-        public ApplicationUser? ShipmentReceivedBy { get; set; }
+        //Nav properties 
+        public List<ArtifactMediaFile> MediaFiles { get; set; } = [];
+        public List<CatalogRecord> CatalogRecords { get; set; } = [];
+
     }
 }
