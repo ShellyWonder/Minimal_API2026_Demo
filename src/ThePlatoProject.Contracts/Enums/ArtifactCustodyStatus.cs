4dee72f881070dddfb2b4f3e3856168b9a464d7a
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2026-08-05 11:02:32 -0500

    Add ArtifactCustodyStatus and SiteLifecycleStatus enums

---
+++ src/ThePlatoProject.Contracts/Enums/ArtifactCustodyStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThePlatoProject.Contracts.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum ArtifactCustodyStatus
+    {
+        OnSite = 1,
+        TransferRequested = 2,
+        TransferAuthorized = 3,
+        InTransit = 4,
+        Warehoused = 5
+
+    }
+}
---
+++ src/ThePlatoProject.Contracts/Enums/SiteLifecycleStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace ThePlatoProject.Contracts.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum SiteLifecycleStatus
+    {
+        Active = 1,
+        ClosureRequested = 2,
+        ClosureAuthorized = 3,
+        Closed = 4
+    }
+}
+
