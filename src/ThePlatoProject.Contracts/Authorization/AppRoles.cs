80ac7ba55dda50e27d9cf0ad02ad1c7415e40b02
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2026-08-10 17:03:25 -0500

    Add AppRoles static class with role constants and list
    Created AppRoles in ThePlatoProject.Contracts.Authorization to define string constants for Admin, Site Manager, Archivist, Field Researcher, Field Staff, and Off-site Personnel roles.

---
+++ src/ThePlatoProject.Contracts/Authorization/AppRoles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThePlatoProject.Contracts.Authorization
+{
+    public static class AppRoles
+    {
+        public const string Admin = "Admin";
+        public const string SiteManager = "Site Manager";
+        public const string Archivist = "Archivist";
+        public const string FieldResearcher = "Field Researcher";
+        public const string FieldStaff = "Field Staff";
+        public const string OffSitePersonnel = "Off-site Personnel";
+        public static readonly List<string> AllRoles = new List<string>
+        {
+            Admin,
+            SiteManager,
+            Archivist,
+            FieldResearcher,
+            FieldStaff,
+            OffSitePersonnel
+        };
+        
+    }
+}
